e87a4af7ba6a4cd53a8a5804d03c0973eb5f1eee
Elad Segal <eladsegal@users.noreply.github.com>
2021-10-29 17:33:31 +0300

    fix for zero length

--- src/datasets/features.py
+++ src/datasets/features.py
@@ -890,11 +890,11 @@ def encode_nested_example(schema, obj):
         if obj is None:
             return None
         else:
-            first_elmt = next(iter(obj))
-            if encode_nested_example(sub_schema, first_elmt) != first_elmt:
-                return [encode_nested_example(sub_schema, o) for o in obj] if obj is not None else None
-            else:
-                return list(obj)
+            if len(obj) > 0:
+                first_elmt = next(iter(obj))
+                if encode_nested_example(sub_schema, first_elmt) != first_elmt:
+                    return [encode_nested_example(sub_schema, o) for o in obj]
+            return list(obj)
     elif isinstance(schema, Sequence):
         # We allow to reverse list of dict => dict of list for compatiblity with tfds
         if isinstance(schema.feature, dict):
@@ -916,9 +916,10 @@ def encode_nested_example(schema, obj):
         if obj is None:
             return None
         else:
-            first_elmt = next(iter(obj))
-            if encode_nested_example(schema.feature, first_elmt) != first_elmt:
-                return [encode_nested_example(schema.feature, o) for o in obj] if obj is not None else None
+            if len(obj) > 0:
+                first_elmt = next(iter(obj))
+                if encode_nested_example(schema.feature, first_elmt) != first_elmt:
+                    return [encode_nested_example(schema.feature, o) for o in obj] if obj is not None else None
             return list(obj)
     # Object with special encoding:
     # ClassLabel will convert from string to int, TranslationVariableLanguages does some checks
